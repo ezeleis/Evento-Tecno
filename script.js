54d9931d524047fc7fb44c8f9db2c16f89b92086
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-02-13 15:45:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var sexo = prompt("Insira M para masculino, F para feminino",);
-var idade = prompt("Insira a idade");
-var estudanteProg = confirm("É estudante de programação ");
-var estudanteLAB = confirm("É estudante do LAB 365");
-
-if (idade < 18 && estudanteProg = false){
-    document.getElementById("Mensagem").innerHTML = "Ingresso não permitido";
-}
+        var idade = prompt("Insira a idade");
+        var estudanteProg = confirm("É estudante de programação ");
+        var estudanteLAB = confirm("É estudante do LAB 365");
+        
+        if(idade < 18 && estudanteProg == false){
+            document.getElementById("Mensagem").innerHTML = "Ingresso não permitido";
+        }
+        else if(idade > 60 || estudanteLAB == true){
+            document.getElementById("Mensagem").innerHTML = "Entrada gratuita";
+        }
+        else if(sexo == "M"){
+            document.getElementById("Mensagem").innerHTML = "Entrada $80";
+        }
+        else if(sexo == "F"){
+            document.getElementById("Mensagem").innerHTML = "Entrada $50";
+        }
