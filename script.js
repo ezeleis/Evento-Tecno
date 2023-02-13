5d4abf0a270b5d46d75ad108b8130d6df6ccd0db
Facundo <facundo@MacBook-Pro-de-Mac-2.local>
2023-02-13 08:54:04 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var sexo = prompt("Insira M para masculino, F para feminino",);
+var idade = prompt("Insira a idade");
+var estudanteProg = confirm("É estudante de programação ");
+var estudanteLAB = confirm("É estudante do LAB 365");
+
+if (idade < 18 && estudanteProg = false){
+    document.getElementById("Mensagem").innerHTML = "Ingresso não permitido";
+}
